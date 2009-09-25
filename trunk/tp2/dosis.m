024f491d94fe2274bf1d808c9171b95386812790
damcho <damcho@1849f666-a3c7-11de-ac05-951101a8bf3e>
2009-09-25 15:04:33 +0000

    va queriendo

--- trunk/tp2/dosis.m
+++ trunk/tp2/dosis.m
@@ -1,38 +1,39 @@
 function [x, y, t] = dosis()
 
-	u 	= zeros(1,70);
-	t 	= [0:0.1:4];
+	u 	= zeros(1,500);		%funcion de imput
+	t 	= [0:0.01:1.5];
 	
-	 x	= [0];
-	 y	= [0];
-	k0	= 0.5;
-	k1	= 2.5;
-	k2	= 3;
-	b0 	= 1.5;
-	h       = 0.1;		% paso 
-	c1	= 30;
-	c2 	= 0;
+	x	= [0];		% vector de concentracion en sangre
+	y	= [0];		% vector de concentracion en ORGANO
+	k0	= 0.5;		% parametro del sistema
+	k1	= 2.5;		% parametro del sistema
+	k2	= 3;		% parametro del sistema
+	b0 	= 1.5;		% parametro del sistema
+	h       = 0.01;		% paso 
+	c1	= 0;		% concentracion en sangre inicial
+	c2 	= 0;		% concentracion en organo inicial
 	i       = 1;
-	tiemp   = 0;
-	K 	= 4;
-		
-	while( tiemp < 4 )
+	tiemp   = 0;		% tiempo en horas
+	K 	= 9;		% Constante K que hay que variar experimentalmente
+	%u(i)	= 60;
 
-		u(i) 	= K* 30 - K * c2;
+	while( tiemp < 1.5 )
+		
+		u(i) 	= ( K * 30 - K * c2 );		% funcion de realimentacion
 	
-		m1 = h * (-( k1 + k0 ) * c1 + b0 * u(i) );
+		m1 = h * (-( k1 + k0 ) * c1 + k2 * c2 + b0 * u(i));
 		n1 = h * (k2 * c1 - k2 * c2 );
 		
-		m2 = h * (-( k1 + k0 ) * ( c1 + m1 / 2 ) + b0 * u(i) );
+		m2 = h * (-( k1 + k0 ) * ( c1 + m1 / 2 ) + k2 * ( c2 + n1 /2 )+ b0 * u(i) );
 		n2 = h * (k2 * ( c1 + m1 /2 ) - k2 * (c2 + n1/ 2) );
 	
-		m3 = h * (-( k1 + k0 ) * ( c1 + m2 / 2 ) + b0 * u(i) );
+		m3 = h * (-( k1 + k0 ) * ( c1 + m2 / 2 ) + k2 * ( c2 + n2 /2 )+ b0 * u(i));
 		n3 = h * (k2 * ( c1 + m2 /2 ) - k2 * (c2 + n2/ 2) );
 
-		m4 = h * (-( k1 + k0 ) * ( c1 + m3 ) + b0 * u(i) );
+		m4 = h * (-( k1 + k0 ) * ( c1 + m3 ) +  k2 * ( c2 + n3  )+ b0 * u(i) );
 		n4 = h * (k2 * ( c1 + m3 ) - k2 * (c2 + n3 ) );
 
-		x = [x; ( c1 + ( m1 + 2*m2 + 2*m3 + m4 ) / 6 )];
+		x = [x; ( c1 + ( m1 + 2*m2 + 2*m3 + m4 ) / 6 )  ];
 		y = [y; ( c2 + ( n1 + 2*n2 + 2*n3 + n4 ) / 6 )];
 
 		tiemp = tiemp + h;
@@ -41,8 +42,7 @@ function [x, y, t] = dosis()
 		c2 = y(i + 1);
 		
 		i++;
-		
 	end
-	plot(t, x, t, y,t, 30,'')
+	plot(t, x, t,y  )
 end
 
