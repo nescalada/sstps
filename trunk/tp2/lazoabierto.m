ed3f153d3469b6902b94a872cead9ad10dc46397
damcho <damcho@1849f666-a3c7-11de-ac05-951101a8bf3e>
2009-09-27 19:32:00 +0000

    ahi va

--- trunk/tp2/lazoabierto.m
+++ trunk/tp2/lazoabierto.m
@@ -1,7 +1,7 @@
 function [x, y, t] = dosis(K)
 
 	u 	= zeros(1,500);		%funcion de imput
-	t 	= [0:0.01:1.5];
+	t 	= [0:0.01:10.01];
 	
 	x	= [0];		% vector de concentracion en sangre
 	y	= [0];		% vector de concentracion en ORGANO
@@ -10,7 +10,7 @@ function [x, y, t] = dosis(K)
 	k2	= 3;		% parametro del sistema
 	b0 	= 1.5;		% parametro del sistema
 	h       = 0.01;		% paso 
-	c1	= 0;		% concentracion en sangre inicial
+	c1	= 30;		% concentracion en sangre inicial
 	c2 	= 0;		% concentracion en organo inicial
 	i       = 1;
 	tiemp   = 0;		% tiempo en horas
@@ -18,20 +18,20 @@ function [x, y, t] = dosis(K)
     
 	%u(i)	= 60;
 
-	while( tiemp < 1.5 )
+	while( tiemp < 10 )
 		
-		u(i) 	= ( K * 30 - K * c2 );		% funcion de realimentacion
+	%	u(i) 	= ( K * 30 - K * c2 );		% funcion de realimentacion
 	
-		m1 = h * (-( k1 + k0 ) * c1 + k2 * c2 + b0 * u(i));
+		m1 = h * (-( k1 + k0 ) * c1 + k1 * c2);
 		n1 = h * (k2 * c1 - k2 * c2 );
 		
-		m2 = h * (-( k1 + k0 ) * ( c1 + m1 / 2 ) + k2 * ( c2 + n1 /2 )+ b0 * u(i) );
+		m2 = h * (-( k1 + k0 ) * ( c1 + m1 / 2 ) + k1 * ( c2 + n1 /2 ) );
 		n2 = h * (k2 * ( c1 + m1 /2 ) - k2 * (c2 + n1/ 2) );
 	
-		m3 = h * (-( k1 + k0 ) * ( c1 + m2 / 2 ) + k2 * ( c2 + n2 /2 )+ b0 * u(i));
+		m3 = h * (-( k1 + k0 ) * ( c1 + m2 / 2 ) + k1 * ( c2 + n2 /2 ));
 		n3 = h * (k2 * ( c1 + m2 /2 ) - k2 * (c2 + n2/ 2) );
 
-		m4 = h * (-( k1 + k0 ) * ( c1 + m3 ) +  k2 * ( c2 + n3  )+ b0 * u(i) );
+		m4 = h * (-( k1 + k0 ) * ( c1 + m3 ) +  k1 * ( c2 + n3  ) );
 		n4 = h * (k2 * ( c1 + m3 ) - k2 * (c2 + n3 ) );
 
 		x = [x; ( c1 + ( m1 + 2*m2 + 2*m3 + m4 ) / 6 )  ];
@@ -44,8 +44,11 @@ function [x, y, t] = dosis(K)
 		
 		i=i+1;
     end
-    hold on
-	%plot(t, x)
-   % plot( t,y  )
+length( x )
+length( y )
+length( t )
+  %  hold on
+	plot(t, x, t,y)
+    
 end
 
