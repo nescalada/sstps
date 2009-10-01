397eb9862edb743601b221696e2f9f1e49db8033
lucilastancato <lucilastancato@1849f666-a3c7-11de-ac05-951101a8bf3e>
2009-10-01 21:45:42 +0000

    para la parte B, hay que ejecutar tp.m y para cambiar los autovalores, cambiarlos en funa1.m.... suerte!

---
+++ tp2/funa1.m
@@ -0,0 +1,23 @@
+function y=funa1(t,X)
+
+lambda1 = -0.83/0.3333333
+lambda2 = -0.6/0.333333
+
+gama1 = (-(lambda1+lambda2)-6)/1.5
+
+gama2 = (lambda1*lambda2)/4.5-gama1
+
+k0 = 0.5;
+k1 = 2.5;
+k2 = 3;
+b0 = 1.5;
+
+A=[ -(k1+k0)-b0*gama1   , k2-b0*gama2;
+    k2                  , -k2  ];
+B=[ b0; 0 ];
+
+
+y= A * X + B*30;
+
+
+end
