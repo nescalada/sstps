25a7e0b48c6e27f332ebbd14230001fa714be69e
cachi77 <cachi77@1849f666-a3c7-11de-ac05-951101a8bf3e>
2009-10-03 23:35:03 +0000

    afasdf

--- trunk/tp2/tp.m
+++ trunk/tp2/tp.m
@@ -1,17 +1,22 @@
 
 
-
-X0=[0  0];
+function tp
+X0=[0 ; 0];
 
 %intervalo de 1 minuto
-tspan=[0 10];
+T=linspace(0,10,60);
 
+SOL1 = lsode( "funa1", X0, T);
 
-[t,Y] = ode45( 'funa1', tspan, X0);
+plot(T, SOL1(:,1))
 
+%intervalo de 1 minuto
+%tspan=[0 10];
 
-size(Y)
-hold on
-plot(t, Y(:,1));
-plot(t, Y(:,2));
+%[t,Y] = ode45( 'funa1', tspan, X0);
 
+%size(Y)
+%hold on
+%plot(t, Y(:,1));
+%plot(t, Y(:,2));
+endfunction
