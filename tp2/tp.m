51c15ceeeb0ccefc114e8f9528838878cf69aa33
lucilastancato <lucilastancato@1849f666-a3c7-11de-ac05-951101a8bf3e>
2009-10-05 05:20:35 +0000

    el grafico de los autovalores

--- tp2/tp.m
+++ tp2/tp.m
@@ -4,19 +4,22 @@ function tp
 X0=[0 ; 0];
 
 %intervalo de 1 minuto
-T=linspace(0,10,60);
+%T=linspace(0,10,60);
+%T=[0 10];
 
-SOL1 = lsode( "funa1", X0, T);
+%SOL1 = lsode( "funa1", X0, T);
 
-plot(T, SOL1(:,1))
+%plot(T, SOL1(:,1))
 
 %intervalo de 1 minuto
-%tspan=[0 10];
+tspan=[0 10];
 
-%[t,Y] = ode45( 'funa1', tspan, X0);
+[t,Y] = ode45( 'funa1', tspan, X0);
 
 %size(Y)
 %hold on
-%plot(t, Y(:,1));
-%plot(t, Y(:,2));
-endfunction
+plot(t, Y(:,1));
+plot(t, Y(:,2));
+%endfunction
+
+end
